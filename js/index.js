d835c073b7d907c74194be0f32b51ee3d24ccf6b
Songjieyu <sjyjsnt@163.com>
2017-11-11 17:05:15 +0800

    Json首页热门排序部分 和 分类热门排序

--- js/index.js
+++ js/index.js
@@ -51,8 +51,8 @@ $(".file ul li").on("click", function () {
         }
     });
 });
-$(".hot ul li").on("click",function () {
-   var fileName = $(this).children("a").children("p").children("span:eq(1)").html();
+$(".hot ul li").on("click", function () {
+    var fileName = $(this).children("a").children("p").children("span:eq(1)").html();
     $.getJSON("json/file.json", function (data) {
         for (var i = 0; i < data.length; i++) {
             if (data[i].name == fileName) {
@@ -62,11 +62,58 @@ $(".hot ul li").on("click",function () {
         }
     });
 });
-$(".more").on("click",function () {
+$(".more").on("click", function () {
     var Type = $(this).attr("name");
-    var obj={
-        "type":Type
+    var obj = {
+        "type": Type
     };
     window.localStorage.setItem('Type', JSON.stringify(obj));
     $(location).attr("href", "show.html");
 });
+
+$.getJSON("json/file.json", function (data) {
+    for (var j = 0; j < 12; j++) {
+        $("div.file ul li:eq(" + j + ") p span").html(data[j]["name"]);
+    }
+    var Varr = [];
+    var Iarr = [];
+    var Darr = [];
+    var Aarr = [];
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].type == "video") {
+            Varr.push(data[i]);
+        } else if (data[i].type == "image") {
+            Iarr.push(data[i]);
+        } else if (data[i].type == "document") {
+            Darr.push(data[i]);
+        } else if (data[i].type == "audio") {
+            Aarr.push(data[i]);
+        }
+    }
+    Sort(Varr);
+    Sort(Iarr);
+    Sort(Darr);
+    Sort(Aarr);
+    for(var i=0;i<10;i++){
+        $("div.hot .Video ul li:eq("+i+") a p span:eq(1)").html(Varr[i]["name"]);
+        $("div.hot .Video ul li:eq("+i+") a p span:eq(2)").html(Varr[i]["Dnum"]);
+        $("div.hot .Audio ul li:eq("+i+") a p span:eq(1)").html(Aarr[i]["name"]);
+        $("div.hot .Audio ul li:eq("+i+") a p span:eq(2)").html(Aarr[i]["Dnum"]);
+        $("div.hot .Image ul li:eq("+i+") a p span:eq(1)").html(Iarr[i]["name"]);
+        $("div.hot .Image ul li:eq("+i+") a p span:eq(2)").html(Iarr[i]["Dnum"]);
+        $("div.hot .Document ul li:eq("+i+") a p span:eq(1)").html(Darr[i]["name"]);
+        $("div.hot .Document ul li:eq("+i+") a p span:eq(2)").html(Darr[i]["Dnum"]);
+    }
+});
+function Sort(Arr) {
+    for (var i = 0; i < Arr.length; i++) {
+        for (var j = 0; j < Arr.length - 1; j++) {
+            if (parseInt(Arr[j]["Dnum"]) < parseInt(Arr[j + 1]["Dnum"])) {
+                var temp = Arr[j];
+                Arr[j] = Arr[j+1];
+                Arr[j+1] = temp;
+            }
+        }
+    }
+    return Arr;
+}
--- js/show.js
+++ js/show.js
@@ -1,22 +1,22 @@
-if(window.localStorage.getItem('Type')){
-    var Stype =window.localStorage.getItem('Type');
-    var SType=JSON.parse(Stype).type;
-    if(SType==="video"){
+if (window.localStorage.getItem('Type')) {
+    var Stype = window.localStorage.getItem('Type');
+    var SType = JSON.parse(Stype).type;
+    if (SType === "video") {
         $(".nav-tabs li:nth-child(1)").addClass("active");
         $(".nav-tabs li:nth-child(1)").siblings().removeClass("active");
         $(".page-header").html("视频");
         window.localStorage.removeItem('Type');
-    }else if(SType==="audio"){
+    } else if (SType === "audio") {
         $(".nav-tabs li:nth-child(2)").addClass("active");
         $(".nav-tabs li:nth-child(2)").siblings().removeClass("active");
         $(".page-header").html("音频");
         window.localStorage.removeItem('Type');
-    }else if(SType==="image"){
+    } else if (SType === "image") {
         $(".nav-tabs li:nth-child(3)").addClass("active");
         $(".nav-tabs li:nth-child(3)").siblings().removeClass("active");
         $(".page-header").html("图片");
         window.localStorage.removeItem('Type');
-    }else if(SType==="document"){
+    } else if (SType === "document") {
         $(".nav-tabs li:nth-child(4)").addClass("active");
         $(".nav-tabs li:nth-child(4)").siblings().removeClass("active");
         $(".page-header").html("文章");
@@ -24,7 +24,6 @@ if(window.localStorage.getItem('Type')){
     }
 
 }
-
 
 
 $(".nav-tabs").children("li:eq(0)").click(function () {
@@ -64,8 +63,7 @@ $(".tab-pane tbody tr").hover(function () {
 });
 
 
-
-$("tbody tr").on("click",function () {
+$("tbody tr").on("click", function () {
     var fileName = $(this).children("td:eq(0)").children("a").html();
     // console.log(fileName);
     $.getJSON("json/file.json", function (data) {
@@ -77,3 +75,72 @@ $("tbody tr").on("click",function () {
         }
     });
 });
+
+$.getJSON("json/file.json", function (data) {
+    var Varr = [];
+    var Iarr = [];
+    var Darr = [];
+    var Zarr = [];
+    var Aarr = [];
+    var Oarr = [];
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].type == "video") {
+            Varr.push(data[i]);
+        }
+        else if (data[i].type == "image") {
+            Iarr.push(data[i]);
+        } else if (data[i].type == "document") {
+            Darr.push(data[i]);
+        } else if (data[i].type == "zip") {
+            Zarr.push(data[i]);
+        } else if (data[i].type == "audio") {
+            Aarr.push(data[i]);
+        } else if (data[i].type == "other") {
+            Oarr.push(data[i]);
+        }
+    }
+    Sort(Varr);
+    Sort(Iarr);
+    Sort(Darr);
+    Sort(Aarr);
+    Sort(Oarr);
+    Sort(Zarr);
+    for(var j=0;j<10;j++){
+        $("div#sp table tbody tr:eq("+j+") td:eq(0) a").html(Varr[j]["name"]);
+        $("div#sp table tbody tr:eq("+j+") td:eq(1)").html(Varr[j]["upuser"]);
+        $("div#sp table tbody tr:eq("+j+") td:eq(2)").html(Varr[j]["uptime"]);
+        $("div#sp table tbody tr:eq("+j+") td:eq(3)").html(Varr[j]["Dnum"]);
+        $("div#yp table tbody tr:eq("+j+") td:eq(0) a").html(Aarr[j]["name"]);
+        $("div#yp table tbody tr:eq("+j+") td:eq(1)").html(Aarr[j]["upuser"]);
+        $("div#yp table tbody tr:eq("+j+") td:eq(2)").html(Aarr[j]["uptime"]);
+        $("div#yp table tbody tr:eq("+j+") td:eq(3)").html(Aarr[j]["Dnum"]);
+        $("div#tp table tbody tr:eq("+j+") td:eq(0) a").html(Iarr[j]["name"]);
+        $("div#tp table tbody tr:eq("+j+") td:eq(1)").html(Iarr[j]["upuser"]);
+        $("div#tp table tbody tr:eq("+j+") td:eq(2)").html(Iarr[j]["uptime"]);
+        $("div#tp table tbody tr:eq("+j+") td:eq(3)").html(Iarr[j]["Dnum"]);
+        $("div#wj table tbody tr:eq("+j+") td:eq(0) a").html(Darr[j]["name"]);
+        $("div#wj table tbody tr:eq("+j+") td:eq(1)").html(Darr[j]["upuser"]);
+        $("div#wj table tbody tr:eq("+j+") td:eq(2)").html(Darr[j]["uptime"]);
+        $("div#wj table tbody tr:eq("+j+") td:eq(3)").html(Darr[j]["Dnum"]);
+        $("div#ysb table tbody tr:eq("+j+") td:eq(0) a").html(Zarr[j]["name"]);
+        $("div#ysb table tbody tr:eq("+j+") td:eq(1)").html(Zarr[j]["upuser"]);
+        $("div#ysb table tbody tr:eq("+j+") td:eq(2)").html(Zarr[j]["uptime"]);
+        $("div#ysb table tbody tr:eq("+j+") td:eq(3)").html(Zarr[j]["Dnum"]);
+        $("div#qt table tbody tr:eq("+j+") td:eq(0) a").html(Oarr[j]["name"]);
+        $("div#qt table tbody tr:eq("+j+") td:eq(1)").html(Oarr[j]["upuser"]);
+        $("div#qt table tbody tr:eq("+j+") td:eq(2)").html(Oarr[j]["uptime"]);
+        $("div#qt table tbody tr:eq("+j+") td:eq(3)").html(Oarr[j]["Dnum"]);
+    }
+});
+function Sort(Arr) {
+    for (var i = 0; i < Arr.length; i++) {
+        for (var j = 0; j < Arr.length - 1; j++) {
+            if (parseInt(Arr[j]["Dnum"]) < parseInt(Arr[j + 1]["Dnum"])) {
+                var temp = Arr[j];
+                Arr[j] = Arr[j+1];
+                Arr[j+1] = temp;
+            }
+        }
+    }
+    return Arr;
+}
